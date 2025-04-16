c7a9e5069bd2be46b8c941b086ce53eab4da5509
cawl0909 <117081278+cawl0909@users.noreply.github.com>
2025-04-16 21:39:31 +0100

    added some classes

---
+++ classes/orbit.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class Orbit
+{
+    private:
+    protected:
+    double eccentricity;
+    double semi_major_axis;
+    double orbital_period;
+    public:
+};
---
+++ classes/planets.h
@@ -0,0 +1,13 @@
+#pragma once 
+
+#include <string>
+
+class Planet
+{
+    private:
+    protected:
+    double mass;
+    double radius;
+    std::string type;
+    public:
+};
